0fb552341bda278cf12c6415592099bbff068083
kunle-lawal <olawal196@gmail.com>
2020-08-01 17:31:58 -0400

    Some blog changes.

--- kunle-blog/src/index.js
+++ kunle-blog/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import '../src/assets/css/sass/main.css';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer';
